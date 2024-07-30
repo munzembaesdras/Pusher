d28dd8869291c237007e4c228101b7023df7185f
munzemba <munzembaesdras@gmail.com>
2024-07-30 18:55:16 +0100

    chore: Mise à jour de la configuration de la connexion MySQL, de la configuration SSH et des dépendances npm

--- server/ticket.js
+++ server/ticket.js
@@ -22,13 +22,13 @@ module.exports = async function (records, connection) {
         ticket_service_name,
       } = record;
 
-      // Vérifier si ticket_status = 1 ou ticket_user_login est null
-      if (ticket_status === 1 || ticket_user_login === null) {
+      // Vérifier si le ticket_status est 1, 0, ou 3
+      if (![1, 0, 3].includes(ticket_status)) {
         console.log(
-          `Le ticket avec le numéro "${ticket_number}" ne sera pas enregistré car le ticket_status est 1 ou ticket_user_login est null.`
+          `Le ticket avec le numéro "${ticket_number}" ne sera pas enregistré car le ticket_status n'est pas 1, 0, ou 3.`
         );
         logger.info(
-          ` Le ticket avec le numéro "${ticket_number}" ne sera pas enregistré car le ticket_status est 1 ou ticket_user_login est null.`
+          `Le ticket avec le numéro "${ticket_number}" ne sera pas enregistré car le ticket_status n'est pas 1, 0, ou 3.`
         );
         continue;
       }
@@ -94,8 +94,9 @@ module.exports = async function (records, connection) {
           error: `Erreur lors de la récupération des IDs nécessaires pour le ticket avec le numéro "${ticket_number}": ${err.message}`,
         };
       }
-      // Insertion dans la table tb_ticket
-      const checkDuplicateSql = `SELECT ticket_number FROM tb_ticket WHERE ticket_number = ? and ticket_agence_nom = ? and ticket_date = ?`;
+
+      // Vérification des doublons et mise à jour si trouvé
+      const checkDuplicateSql = `SELECT ticket_number FROM tb_ticket WHERE ticket_number = ? AND ticket_agence_nom = ? AND ticket_date = ?`;
 
       try {
         const [duplicateRows] = await connection.execute(checkDuplicateSql, [
@@ -105,14 +106,126 @@ module.exports = async function (records, connection) {
         ]);
 
         if (duplicateRows.length > 0) {
-          logger.info(
-            `Le ticket avec le numéro "${ticket_number}" existe déjà dans la base de données.`
-          );
-          console.log(
-            `Le ticket avec le numéro "${ticket_number}" existe déjà dans la base de données.`
-          );
-          // Vous pouvez choisir d'effectuer une mise à jour ici si nécessaire.
-          continue; // Passez au ticket suivant sans effectuer d'insertion.
+          // Ticket déjà existant, faire une mise à jour
+          const updateSql = `UPDATE tb_ticket SET
+            ticket_heure_debut = ?,
+            ticket_heure_fin = ?,
+            ticket_date2 = ?,
+            ticket_service_id = ?,
+            ticket_guichet = ?,
+            ticket_user_id = ?,
+            ticket_status = ?,
+            ticket_client_number = ?,
+            ticket_is_vip = ?,
+            ticket_raison_id = ?,
+            ticket_agence_id = ?,
+            ticket_syncronized = ?,
+            ticket_user_login = ?,
+            ticket_service_name = ?
+          WHERE ticket_number = ? AND ticket_agence_nom = ? AND ticket_date = ?`;
+
+          const updateParams = [
+            ticket_heure_debut,
+            ticket_heure_fin,
+            new Date(ticket_date2).toISOString().slice(0, 19),
+            ticket_service_id,
+            ticket_guichet,
+            ticket_user_id,
+            ticket_status,
+            ticket_client_number,
+            ticket_is_vip,
+            ticket_raison_id,
+            ticket_agence_id,
+            ticket_syncronized,
+            ticket_user_login,
+            ticket_service_name,
+            ticket_number,
+            ticket_agence_nom,
+            new Date(ticket_date).toISOString().slice(0, 19),
+          ];
+
+          try {
+            await connection.execute(updateSql, updateParams);
+            logger.info(
+              `Le ticket avec le numéro "${ticket_number}" a été mis à jour avec succès.`
+            );
+            console.log(
+              `Le ticket avec le numéro "${ticket_number}" a été mis à jour avec succès.`
+            );
+          } catch (err) {
+            logger.error(
+              `Erreur lors de la mise à jour du ticket avec le numéro "${ticket_number}": ${err.message}`
+            );
+            console.error(
+              `Erreur lors de la mise à jour du ticket avec le numéro "${ticket_number}":`,
+              err
+            );
+            return {
+              error: `Erreur lors de la mise à jour du ticket avec le numéro "${ticket_number}": ${err}`,
+            };
+          }
+        } else {
+          // Aucun doublon trouvé, insérer un nouveau ticket
+          const insertSql = `INSERT INTO tb_ticket (
+            ticket_date,
+            ticket_heure_debut,
+            ticket_heure_fin,
+            ticket_date2,
+            ticket_number,
+            ticket_service_id,
+            ticket_guichet,
+            ticket_user_id,
+            ticket_status,
+            ticket_client_number,
+            ticket_is_vip,
+            ticket_raison_id,
+            ticket_agence_id,
+            ticket_agence_nom,
+            ticket_syncronized,
+            ticket_user_login,
+            ticket_service_name
+          ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`;
+
+          const insertParams = [
+            new Date(ticket_date).toISOString().slice(0, 19),
+            ticket_heure_debut,
+            ticket_heure_fin,
+            new Date(ticket_date2).toISOString().slice(0, 19),
+            ticket_number,
+            ticket_service_id,
+            ticket_guichet,
+            ticket_user_id,
+            ticket_status,
+            ticket_client_number,
+            ticket_is_vip,
+            ticket_raison_id,
+            ticket_agence_id,
+            ticket_agence_nom,
+            ticket_syncronized,
+            ticket_user_login,
+            ticket_service_name,
+          ];
+
+          try {
+            await connection.execute(insertSql, insertParams);
+            logger.info(
+              `Le ticket avec le numéro "${ticket_number}" a été enregistré avec succès.`
+            );
+            console.log(
+              `Le ticket avec le numéro "${ticket_number}" a été enregistré avec succès.`
+            );
+          } catch (err) {
+            logger.error(
+              `Erreur lors de l'insertion du ticket avec le numéro "${ticket_number}": ${err.message}`
+            );
+            console.error(
+              `Erreur lors de l'insertion du ticket avec le numéro "${ticket_number}":`,
+              err
+            );
+            return {
+              error: `Erreur lors de l'insertion du ticket avec le numéro "${ticket_number}": ${err}`,
+            };
+          }
         }
       } catch (err) {
         logger.error(
@@ -126,80 +239,15 @@ module.exports = async function (records, connection) {
           error: `Erreur lors de la vérification de la duplication pour le ticket avec le numéro "${ticket_number}": ${err.message}`,
         };
       }
-
-      // Si aucun ticket en double n'est trouvé, procédez à l'insertion normale.
-      const sql = `INSERT INTO tb_ticket (
-    ticket_date,
-    ticket_heure_debut,
-    ticket_heure_fin,
-    ticket_date2,
-    ticket_number,
-    ticket_service_id,
-    ticket_guichet,
-    ticket_user_id,
-    ticket_status,
-    ticket_client_number,
-    ticket_is_vip,
-    ticket_raison_id,
-    ticket_agence_id,
-    ticket_agence_nom,
-    ticket_syncronized,
-    ticket_user_login,
-    ticket_service_name
-  ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`;
-
-      const params = [
-        new Date(ticket_date).toISOString().slice(0, 19),
-        ticket_heure_debut,
-        ticket_heure_fin,
-        new Date(ticket_date).toISOString().slice(0, 19),
-        ticket_number,
-        ticket_service_id,
-        ticket_guichet,
-        ticket_user_id,
-        ticket_status,
-        ticket_client_number,
-        ticket_is_vip,
-        ticket_raison_id,
-        ticket_agence_id,
-        ticket_agence_nom,
-        ticket_syncronized,
-        ticket_user_login,
-        ticket_service_name,
-      ];
-
-      try {
-        await connection.execute(sql, params);
-        logger.info(
-          `Le ticket avec le numéro "${ticket_number}" a été enregistré avec succès.`
-        );
-        console.log(
-          `Le ticket avec le numéro "${ticket_number}" a été enregistré avec succès.`
-        );
-      } catch (err) {
-        logger.error(
-          `Erreur lors de l'insertion du ticket avec le numéro "${ticket_number}": ${err.message}`
-        );
-        console.error(
-          `Erreur lors de l'insertion du ticket avec le numéro "${ticket_number}":`,
-          err
-        );
-        return {
-          error: `Erreur lors de l'insertion du ticket avec le numéro "${ticket_number}": ${err}`,
-        };
-      }
     }
   } catch (err) {
     console.error(
-      "Erreur lors du traitement des enregistrements: ",
+      "Erreur inattendue lors du traitement des tickets:",
       err.message
     );
-
+    logger.error(`Erreur inattendue lors du traitement des tickets: ${err.message}`);
     return {
-      error: `Erreur lors du traitement des enregistrements: ${err.message}`,
+      error: `Erreur inattendue lors du traitement des tickets: ${err.message}`,
     };
-  } finally {
-    await connection.end();
   }
-  return { success: true };
 };
