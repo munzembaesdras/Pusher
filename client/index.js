b3e5fc568872399cae81eab5b96773b25cf4be9d
munzemba <munzembaesdras@gmail.com>
2024-08-30 20:12:53 +0100

    bande d'annonce

--- client/index.js
+++ client/index.js
@@ -2,8 +2,8 @@ const express = require("express");
 const mysql = require("mysql2/promise");
 const axios = require("axios");
 const cron = require("node-cron");
-const {  dbConfig, dbConfigClientTemplate } = require("../config");
-const service = require("./service");
+const { dbConfig, dbConfigClientTemplate } = require("../config");
+//const service = require("./service");
 const user = require("../server/user");
 const partenaire = require("./partenaire");
 const role = require("./role");
@@ -11,16 +11,17 @@ const role_user = require("../server/role_user");
 const moment = require("moment");
 const bodyParser = require("body-parser");
 const logger = require("../log");
-const video = require('./video');
+const video = require("./video");
 
 const app = express();
-const PORT = 3006;
-const wehereTciket = "SELECT * FROM tb_ticket WHERE ticket_date2 >= NOW() - INTERVAL 3 DAY";
+const PORT = 3008;
+const wehereTciket =
+  "SELECT * FROM tb_ticket WHERE ticket_date2 >= NOW() - INTERVAL 1 DAY";
 let JSONs;
 app.use(bodyParser.json({ limit: "100mb" }));
 
 // Route pour recevoir les données des clients
-app.post("/Pusher/sync", async (req, res) => {
+/* app.post("/Pusher/sync", async (req, res) => {
   const { data } = req.body;
   logger.info("Données reçues du client:", data);
 
@@ -68,6 +69,8 @@ app.post("/Pusher/sync", async (req, res) => {
   res.sendStatus(200);
 });
 
+*/
+
 // RECUPERATION DES DONNEES DE LA BASE DE DONNEES CENTRALE
 const getDbConnection = async (config) => {
   const connection = await mysql.createConnection(config);
@@ -79,21 +82,23 @@ const getClientDbConfigs = async () => {
   try {
     connection = await getDbConnection(dbConfig); // Connexion au serveur central
     const [agencies] = await connection.query(
-      "SELECT agence_ip, agence_nom FROM tb_agence" // Récupérer les adresses des bases de données des agences
+      "SELECT agence_ip, agence_nom FROM tb_agence WHERE agence_ip != '10.56.11.28' ORDER BY agence_ip;" // Récupérer les adresses des bases de données des agences
     );
     return agencies.map((agency) => ({
       host: agency.agence_ip,
       ...dbConfigClientTemplate, // Les infos utilisateur et mot de passe restent les mêmes
     }));
   } catch (error) {
-    logger.error("Erreur de récupération des configurations des bases de données des agences:", error);
+    logger.error(
+      "Erreur de récupération des configurations des bases de données des agences:",
+      error
+    );
     throw error;
   } finally {
     if (connection) await connection.end();
   }
 };
 
-// SYNCHRONISATION DES DONNEES DES AGENCES
 const syncDataFromAgencies = async (contrainte) => {
   try {
     const clientDbConfigs = await getClientDbConfigs();
@@ -102,68 +107,113 @@ const syncDataFromAgencies = async (contrainte) => {
       let connection;
       try {
         connection = await getDbConnection(clientConfig);
-
-        // Récupérer les données depuis la base de données de l'agence
-        const [tickets] = await connection.query(contrainte);
-        const [users] = await connection.query("SELECT * FROM tb_users");
-
-        // Format the date fields in users
-        users.forEach((user) => {
-          user.creation_date = moment(user.creation_date).format("YYYY-MM-DD HH:mm:ss");
-        });
-
-        const [role_user] = await connection.query(
-          "SELECT u.user_login, r.role_nom FROM tb_role_user ru JOIN tb_users u ON ru.user_id=u.user_id JOIN tb_role r ON ru.role_id=r.role_id"
+        logger.info(
+          `Connexion à la base de données de l'agence ${clientConfig.host} établie.`
         );
 
-        const tables = [
-          { table: "tb_users", records: users },
-          { table: "tb_ticket", records: tickets },
-          { table: "tb_role_user", records: role_user },
-        ];
-        const Data = {
-          data: tables,
-        };
-        JSONs = { Data };
-        logger.info("Données à envoyer aux serveurs:", JSONs);
+        // Vérifier si la contrainte est définie et est une chaîne non vide
+        if (typeof contrainte === "string" && contrainte.trim().length > 0) {
+          logger.info(
+            `Exécution de la requête avec la contrainte: ${contrainte}`
+          );
 
-        // Exécuter le traitement des données ou les envoyer ailleurs
-        await processAgencyData(tables, clientConfig.host, Data);
-        
+          // Récupérer les données en utilisant la contrainte
+          const [tickets] = await connection.query(
+            "SELECT * FROM tb_ticket WHERE ticket_date2 >= NOW() - INTERVAL 1 DAY"
+          );
+
+          // Récupération des utilisateurs associés aux tickets récents
+          const [users] = await connection.query(
+            `SELECT u.* FROM tb_ticket t JOIN tb_users u ON t.ticket_user_login = u.user_login WHERE t.ticket_date2 BETWEEN DATE_SUB(CURDATE(), INTERVAL 1 WEEK) AND CURDATE() GROUP BY u.user_login ORDER BY u.user_login ASC;`
+          );
+
+          // Formater les dates des utilisateurs
+          users.forEach((user) => {
+            user.creation_date = moment(user.creation_date).format(
+              "YYYY-MM-DD HH:mm:ss"
+            );
+          });
+
+          // Récupération des rôles des utilisateurs
+          const [role_user] = await connection.query(`
+            SELECT u.user_login, r.role_nom FROM tb_role_user ru 
+			      JOIN tb_users u ON ru.user_id = u.user_id 
+            JOIN tb_role r ON ru.role_id = r.role_id 
+			      JOIN tb_ticket t ON t.ticket_user_id = u.user_id  WHERE t.ticket_date2 BETWEEN DATE_SUB(CURDATE(), INTERVAL 1 WEEK) AND CURDATE()
+			      GROUP BY u.user_id ASC;`);
+
+          // Préparer les données à envoyer
+          const tables = [
+            { table: "tb_users", records: users },
+            { table: "tb_ticket", records: tickets },
+            { table: "tb_role_user", records: role_user },
+          ];
+
+          const Data = {
+            data: tables,
+          };
+          JSONs = { Data };
+          logger.info("Données à envoyer aux serveurs:", JSONs);
+
+          // Traiter les données récupérées
+          await processAgencyData(tables, clientConfig.host, Data);
+        } else {
+          logger.error(
+            `La contrainte est indéfinie ou invalide pour l'agence ${clientConfig.host}`
+          );
+        }
       } catch (error) {
-        logger.error(`Erreur lors de la connexion à la base de données de l'agence ${clientConfig.host}:`, error);
+        logger.error(
+          `Erreur lors de la connexion ou de la requête pour l'agence ${clientConfig.host}:`,
+          error
+        );
       } finally {
         if (connection) {
           await connection.end();
+          logger.info(
+            `Connexion à la base de données de l'agence ${clientConfig.host} fermée.`
+          );
         }
       }
     }
   } catch (error) {
-    logger.error("Erreur lors de la synchronisation des données des agences:", error);
+    logger.error(
+      "Erreur lors de la synchronisation des données des agences:",
+      error
+    );
   }
 };
 
 // TRAITEMENT DES DONNEES RECUPEREES
 const processAgencyData = async (tables, agencyHost, Data) => {
-  // Ici, tu peux implémenter le code pour traiter les données récupérées
-  // ou les envoyer à un autre serveur via axios par exemple
-
-      await axios.post(`http://localhost:3005/Pusher/sync`, Data);
-      logger.info(
-        `Données envoyées au serveur localhost pour la table $.`
-      );
-  logger.info(`Données récupérées et envoyée pour l'agence ${agencyHost}`);
+  try {
+    // Envoyer les données au serveur distant
+    await axios.post(`http://localhost:3007/Pusher/sync`, Data);
+    logger.info(
+      `Données envoyées au serveur localhost pour l'agence ${agencyHost}.`
+    );
+  } catch (error) {
+    logger.error(
+      `Erreur lors de l'envoi des données pour l'agence ${agencyHost}:`,
+      error
+    );
+  }
 };
 
 // Route pour déclencher la synchronisation manuellement
 app.get("/Pusher/sync", async (req, res) => {
   try {
-    await syncDataFromAgencies("SELECT * FROM tb_ticket WHERE YEAR(ticket_date2) = YEAR(CURDATE());");
+    await syncDataFromAgencies(
+      "SELECT * FROM tb_ticket WHERE YEAR(ticket_date2) = YEAR(CURDATE());"
+    );
     res.sendStatus(200);
     logger.info("Synchronisation des données des agences réussie.");
   } catch (error) {
     res.sendStatus(500);
-    logger.error("Erreur lors de la synchronisation des données des agences:", error);
+    logger.error(
+      "Erreur lors de la synchronisation des données des agences:",
+      error
+    );
   }
 });
 
@@ -171,5 +221,9 @@ app.listen(PORT, () => {
   logger.info(`Le serveur s'exécute sur le port ${PORT}`);
   syncDataFromAgencies(wehereTciket);
   // Synchronize data every 40 minutes between 8 AM and 5 PM from Monday to Saturday
-  cron.schedule("*/40 8-17 * * 1-6", syncDataFromAgencies);
+  cron.schedule("*/60 9,12,15,17,18 * * 1-6", () => {
+    syncDataFromAgencies(
+      "SELECT * FROM tb_ticket WHERE ticket_date2 >= NOW() - INTERVAL 1 DAY"
+    );
+  });
 });
--- server/index.js
+++ server/index.js
@@ -10,9 +10,9 @@ const moment = require("moment");
 const bodyParser = require("body-parser");
 const logger = require("../log");
 const app = express();
-const PORT = 3005;
-let JSONs;
+const PORT = 3007;
 let sentData = [];
+const annonce = require("./annonce");
 
 app.use(bodyParser.json({ limit: "100mb" }));
 
@@ -34,10 +34,15 @@ app.post("/Pusher/sync", async (req, res) => {
         await role_user(records, connection);
       } else if (table === "tb_ticket") {
         await ticket(records, connection);
+      } else if (table === "tb_bande_annonce") {
+        await annonce(records, connection);
       }
       logger.info(`Données de la table ${table} traitées avec succès.`);
     } catch (error) {
-      logger.error(`Erreur de traitement de donnée de la table ${table}:`, error);
+      logger.error(
+        `Erreur de traitement de donnée de la table ${table}:`,
+        error
+      );
     } finally {
       await connection.end();
       logger.info(`Connexion à la base de données fermée.`);
@@ -54,20 +59,38 @@ const syncDataToClients = async () => {
 
     // Récupération des données des tables
     const [agences] = await connection.query("SELECT * FROM tb_agence");
-    const [role] = await connection.query("SELECT r.role_nom, r.role_status, p.partenairenom FROM tb_role r JOIN tb_partenaire p ON r.partenaire_id = p.partenaireid;");
+    const [role] = await connection.query(
+      "SELECT r.role_nom, r.role_status, p.partenairenom FROM tb_role r JOIN tb_partenaire p ON r.partenaire_id = p.partenaireid;"
+    );
     const [users] = await connection.query("SELECT * FROM tb_users");
-    users.forEach(user => {
-      user.creation_date = moment(user.creation_date).format("YYYY-MM-DD HH:mm:ss");
+    const [annonces] = await connection.query("SELECT * FROM tb_bande_annonce");
+
+    users.forEach((user) => {
+      user.creation_date = moment(user.creation_date).format(
+        "YYYY-MM-DD HH:mm:ss"
+      );
     });
-    const [role_user] = await connection.query("SELECT u.user_login, r.role_nom FROM tb_role_user ru JOIN tb_users u ON ru.user_id=u.user_id JOIN tb_role r ON ru.role_id=r.role_id");
-    const [videos_agence] = await connection.query("SELECT va.agence_id, a.agence_ip, v.* FROM tb_video_agence va JOIN tb_agence a ON a.agence_id = va.agence_id JOIN tb_video v ON v.video_id = va.video_id");
-    videos_agence.forEach(video => {
-      video.video_create_date = moment(video.video_create_date).format("YYYY-MM-DD HH:mm:ss");
-      video.video_modify_date = moment(video.video_modify_date).format("YYYY-MM-DD HH:mm:ss");
+    const [role_user] = await connection.query(
+      "SELECT u.user_login, r.role_nom FROM tb_role_user ru JOIN tb_users u ON ru.user_id=u.user_id JOIN tb_role r ON ru.role_id=r.role_id"
+    );
+    const [videos_agence] = await connection.query(
+      "SELECT va.agence_id, a.agence_ip, v.* FROM tb_video_agence va JOIN tb_agence a ON a.agence_id = va.agence_id JOIN tb_video v ON v.video_id = va.video_id"
+    );
+    videos_agence.forEach((video) => {
+      video.video_create_date = moment(video.video_create_date).format(
+        "YYYY-MM-DD HH:mm:ss"
+      );
+      video.video_modify_date = moment(video.video_modify_date).format(
+        "YYYY-MM-DD HH:mm:ss"
+      );
     });
-    const [services_agence] = await connection.query("SELECT sa.agence_id, a.agence_ip, s.* FROM tb_service_agence sa JOIN tb_agence a ON a.agence_id = sa.agence_id JOIN tb_service s ON s.service_id = sa.service_id");
-    services_agence.forEach(service => {
-      service.service_create_date = moment(service.service_create_date).format("YYYY-MM-DD HH:mm:ss");
+    const [services_agence] = await connection.query(
+      "SELECT sa.agence_id, a.agence_ip, s.* FROM tb_service_agence sa JOIN tb_agence a ON a.agence_id = sa.agence_id JOIN tb_service s ON s.service_id = sa.service_id"
+    );
+    services_agence.forEach((service) => {
+      service.service_create_date = moment(service.service_create_date).format(
+        "YYYY-MM-DD HH:mm:ss"
+      );
     });
 
     // Récupération des adresses IP des clients
@@ -75,8 +98,10 @@ const syncDataToClients = async () => {
       let connection;
       try {
         connection = await getDbConnection();
-        const [rows] = await connection.query("SELECT agence_ip FROM tb_agence");
-        return rows.map(row => row.agence_ip);
+        const [rows] = await connection.query(
+          "SELECT agence_ip FROM tb_agence"
+        );
+        return rows.map((row) => row.agence_ip);
       } catch (error) {
         logger.error("Erreur de récupération des IPs des clients:", error);
         throw error;
@@ -99,6 +124,7 @@ const syncDataToClients = async () => {
 
         const tables = [
           { table: "tb_agence", records: agences },
+          { table: "tb_bande_annonce", records: annonces },
           { table: "tb_service", records: filteredServices },
           { table: "tb_role", records: role },
           { table: "tb_users", records: users },
@@ -113,7 +139,7 @@ const syncDataToClients = async () => {
 
         sentData.push(Data);
 
-        await axios.post(`http://${clientIp}:3006/Pusher/sync`, Data);
+        // await axios.post(`http://${clientIp}:3006/Pusher/sync`, Data);
       } catch (error) {
         logger.error(`Erreur d'envoi de données au client ${clientIp}:`, error);
       }
@@ -129,12 +155,13 @@ const syncDataToClients = async () => {
   }
 };
 
-
 app.get("/Pusher/sync", (req, res) => {
   try {
     syncDataToClients();
     if (sentData.length === 0) {
-      res.status(404).json({ message: "No data has been sent to clients yet." });
+      res
+        .status(404)
+        .json({ message: "No data has been sent to clients yet." });
     } else {
       res.json(sentData);
     }
